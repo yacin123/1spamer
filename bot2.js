8779ad91f04913da7e651a6cbffb9a9478afb65d
yacin123 <44523421+yacin123@users.noreply.github.com>
2018-11-17 17:16:57 +0100

    Create bot2.js

---
+++ bot2.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const other = new Discord.Client();
+
+other.on('ready', () => {
+  console.log(`Logged in as ${other.user.tag}!`);
+});
+
+other.on('ready', async () => {
+      let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
+  setInterval(() => {
+other.channels.get("510204199749943296").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+},300);
+});
+
+other.login("NTAwMDExNTQyNjc1MzI0OTM5.DtHPkw.CnGwbDwjAuY_wTGsq00-Wdd8hMg")
