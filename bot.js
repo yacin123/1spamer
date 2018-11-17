d255637ab1d57fd311e7ce2e78fbc1987aa8a52b
yacin123 <44523421+yacin123@users.noreply.github.com>
2018-11-17 10:51:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,15 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const other = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
+other.on('ready', () => {
+  console.log(`Logged in as ${other.user.tag}!`);
 });
 
+other.on('ready', async () => {
+      let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
+  setInterval(() => {
+other.channels.get("513285599470551051").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+},300);
+});
 
-client.on('ready', async() => {
-var server = "Id server"; //510860891496120321
-var channel = "id room";//513285599470551051
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam ,  , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , Dalida Spam , **')
-    },400);
-})
-
-client.login(NDgzNjg2MTcyMjIxMjQzNDAy.DtFigg.2OYRrIM8CgjfNkQpoNZnTPxjto4);
+other.login("NDgzNjg2MTcyMjIxMjQzNDAy.DtFigg.2OYRrIM8CgjfNkQpoNZnTPxjto4")
