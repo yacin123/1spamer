50c35709a4369d4c53036e7aac2e5a3d8c644977
yacin123 <44523421+yacin123@users.noreply.github.com>
2018-11-23 09:56:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,8 @@ other.on('ready', () => {
 other.on('ready', async () => {
       let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
   setInterval(() => {
-other.channels.get("513285599470551051").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+other.channels.get("507653890205024258").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
 },300);
 });
 
-other.login("NDgzNjg2MTcyMjIxMjQzNDAy.DtFigg.2OYRrIM8CgjfNkQpoNZnTPxjto4")
+other.login("NTEyNjg4ODIxMDA4MzM0ODUw.DtlRwg.h6oj-Ui9yJfZ5gDP1frDPUiw-vw")
